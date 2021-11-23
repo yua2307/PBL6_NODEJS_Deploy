77d9f8ed75933c491969ad969c442492b91ace38
vanchinhduong <vanchinhduong2307@gmail.com>
2021-11-23 12:59:53 +0700

    fix port start

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ db.sequelize.sync().then(() => {
 app.use(userResumesRoute);
 app.use(emailRoute);
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000 || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
